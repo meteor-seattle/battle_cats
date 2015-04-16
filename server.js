eb543db960e1abfd6e810c3528e81a64cbb9e83e
Ryan Sobol <contact@ryansobol.com>
2015-04-15 22:16:09 -0700

    Add more express routes

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
+
+app.get('/secret', function (req, res) {
+  res.send('No body here but us chickens.');
+})
+
+app.use(function(req, res, next) {
+  res.status(404).sendFile(__dirname + '/public/404.html');
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
