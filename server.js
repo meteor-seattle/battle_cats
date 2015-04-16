f7cb3227bcb9f1970f2fa26a829fac06089a452a
Ryan Sobol <contact@ryansobol.com>
2015-04-16 10:56:04 -0700

    Improve the code for teaching

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/secret', function (req, res) {
-  res.send('No body here but us chickens.');
+app.get('/secret', function(request, response) {
+  response.send('No body here but us chickens.');
 })
 
-app.use(function(req, res, next) {
-  res.status(404).sendFile(__dirname + '/public/404.html');
+app.use(function(request, response, next) {
+  response.status(404).sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log('Node app is running at localhost:' + app.get('port'));
 });
